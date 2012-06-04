12c7da457b074a43abdc9f0bd51b2972ce1b2853
Rusty Zarse <rusty.zarse@searchdiscovery.com>
2012-06-03 22:02:18 -0400

    updated home controller so it can handle old data missing user.person

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   skip_before_filter :require_login 
   
   def index
-    @goals = Goal.by_person_as_student(current_user.person) if current_user
+    @goals = Goal.by_person_as_student(current_user.person) if current_user && current_user.person
   end
   
   def about
