ff5f80c44ebf5a774401c91b0af13777e3aae844
Rusty Zarse <rusty@levo.us>
2012-06-05 09:39:01 -0400

    change to test whether my remote url update worked

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -5,7 +5,7 @@ describe Goal do
     Goal.new.should be_an_instance_of(Goal)
   end
 
-  it "can be saved successfully" do
+  it "can be saved successfullysh" do
     Goal.create.should be_persisted
   end
 end
