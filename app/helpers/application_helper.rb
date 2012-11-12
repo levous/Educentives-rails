aefb32f5b26d7067aad3a6ebd0362dee0b1b4740
Rusty Zarse <rusty@levo.us>
2012-11-12 13:15:05 -0500

    added custom form builder for nice, dry styling.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  class StyledFormBuilder < ActionView::Helpers::FormBuilder
+    def submit(options={})
+      css_class = options[:class].nil? ? 'btn' : options[:class] + ' btn'
+      super :class => css_class
+    end
+  end
+  ActionView::Base.default_form_builder = StyledFormBuilder
 end
